fa2f4571f33ef7d9a6d83eb660b7c8a2a385d468
Aatish Agarwal <aagarwal@symbotic.com>
2025-05-30 19:28:33 +0530

    Added pusher metrics project

---
+++ PushingMetrics/MetricsCollector.cs
@@ -0,0 +1,37 @@
+﻿using Prometheus;
+
+public class MetricsCollector
+{
+    private readonly Counter _counter;
+    private readonly Gauge _gauge;
+    private readonly Histogram _histogram;
+
+    public MetricsCollector(CollectorRegistry registry)
+    {
+        _counter = Metrics.WithCustomRegistry(registry).CreateCounter("demo_requests_total", "Total number of requests.");
+        _gauge = Metrics.WithCustomRegistry(registry).CreateGauge("demo_in_progress_gauge", "Current number of in-progress tasks.");
+        _histogram = Metrics.WithCustomRegistry(registry).CreateHistogram("demo_request_duration_seconds", "Histogram of request durations.");
+    }
+
+    public async Task SimulateMetrics(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            _counter.Inc();
+
+            _gauge.Inc();
+
+            Random rand = new Random();
+            double simulatedDuration = rand.NextDouble();
+            using (_histogram.NewTimer())
+            {
+                await Task.Delay((int)(simulatedDuration * 1000), cancellationToken);
+            }
+
+            _gauge.Dec();
+
+            Console.WriteLine("Simulated metrics at " + DateTime.Now);
+            await Task.Delay(2000); // Every 2 seconds
+        }
+    }
+}
---
+++ PushingMetrics/Program.cs
@@ -0,0 +1,32 @@
+﻿using Prometheus;
+
+Console.WriteLine("Starting Prometheus Metrics Pusher...");
+
+var registry = Metrics.NewCustomRegistry();
+
+var pusher = new MetricPusher(
+    endpoint: "http://localhost:9091/metrics",
+    job: "dotnet_push_metrics",
+    instance: Environment.MachineName,
+    intervalMilliseconds: 5000,
+    registry: registry
+);
+
+pusher.Start();
+
+// Start metrics server (listen on http://localhost:1234/metrics)
+////var server = new KestrelMetricServer(port: 1234);
+////server.Start();
+
+var collector = new MetricsCollector(registry);
+var cts = new CancellationTokenSource();
+
+AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+{
+    Console.WriteLine("Shutting down...");
+    cts.Cancel();
+    pusher.Stop();
+};
+
+// Simulate metrics in loop
+await collector.SimulateMetrics(cts.Token);
