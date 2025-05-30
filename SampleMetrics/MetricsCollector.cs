b346109a3b06d59c93333fa1401aae9052c306fe
Aatish Agarwal <aagarwal@symbotic.com>
2025-05-30 17:40:59 +0530

    Add project files.

---
+++ SampleMetrics/MetricsCollector.cs
@@ -0,0 +1,31 @@
+﻿using Prometheus;
+
+public class MetricsCollector
+{
+    private readonly Counter _counter;
+    private readonly Gauge _gauge;
+    private readonly Histogram _histogram;
+
+    public MetricsCollector()
+    {
+        _counter = Metrics.CreateCounter("demo_requests_total", "Total number of requests.");
+        _gauge = Metrics.CreateGauge("demo_in_progress_gauge", "Current number of in-progress tasks.");
+        _histogram = Metrics.CreateHistogram("demo_request_duration_seconds", "Histogram of request durations.");
+    }
+
+    public void SimulateMetrics()
+    {
+        _counter.Inc();
+
+        _gauge.Inc();
+
+        Random rand = new Random();
+        double simulatedDuration = rand.NextDouble();
+        using (_histogram.NewTimer())
+        {
+            Thread.Sleep((int)(simulatedDuration * 1000));
+        }
+
+        _gauge.Dec();
+    }
+}
---
+++ SampleMetrics/Program.cs
@@ -0,0 +1,17 @@
+﻿using Prometheus;
+
+Console.WriteLine("Starting Prometheus Metrics Collector...");
+
+// Start metrics server (listen on http://localhost:1234/metrics)
+var server = new KestrelMetricServer(port: 1234);
+server.Start();
+
+var collector = new MetricsCollector();
+
+// Simulate metrics in loop
+while (true)
+{
+    collector.SimulateMetrics();
+    Console.WriteLine("Simulated metrics at " + DateTime.Now);
+    Thread.Sleep(2000); // Every 2 seconds
+}
